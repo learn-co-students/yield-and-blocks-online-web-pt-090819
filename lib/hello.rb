2a34f3013eb8ae05e16a49f43d0bec347e44fce4
Janay Bailey <tjanay87@gmail.com>
2019-09-23 15:04:40 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,26 @@
-def hello_t
+require "pry"
 
+def hello_t(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
-# call your method here!
+# call your metho here!
 
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
